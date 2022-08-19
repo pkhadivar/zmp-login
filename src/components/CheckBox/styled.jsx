962c8be74599bc38aa2bb5ae2bd1f0ba64a7e6e9
Max.a <Max.a@realtyna.net>
2022-08-19 13:57:50 +0430

    write example styled components

---
+++ src/components/CheckBox/styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-component";
+import { Checkbox } from "@material-ui/core";
+
+export const FormCheckBoxField = styled(Checkbox)`
+  ${(props) => props.styled};
+`;
+
+// export const
