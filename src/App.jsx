2f7632019f761aee3feb9448eb08893f2e3b31a0
POURIA <p.khadivar@gmail.com>
2022-08-27 11:27:34 +0430

    Login2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Login from "./pages/login/Login";
+import Login2 from "./pages/login/Login2"
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Login2 />
     </div>
   );
 }
